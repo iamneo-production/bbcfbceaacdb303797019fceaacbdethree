d40ed921bd460ad6818e11e000bc412cce6b696d
Iamneo <techadmin@iamneo.ai>
2023-08-02 20:45:13 +0000

    cdfas

---
+++ src/index.js
@@ -0,0 +1,99 @@
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+console.log("GFasg")
+
+
+for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}
+
+for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}for(let i = 0; i <= 1000; i++) {
+    console.log("fasg")
+}
